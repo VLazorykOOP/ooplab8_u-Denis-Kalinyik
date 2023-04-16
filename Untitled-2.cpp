76adcdb6eb231c57b02bd64ddef624766fd8d052
Denis-Kalinyik <99794679+Denis-Kalinyik@users.noreply.github.com>
2023-04-16 23:32:12 +0300

    Add files via upload

--- Untitled-2.cpp
+++ Untitled-2.cpp
@@ -1,46 +1,29 @@
 #include <iostream>
-#include <algorithm>
-#include <cstring>
 
-template <typename T>
-void bubbleSort(T arr[], int n) {
+using namespace std;
+
+const int m = 7;
+
+template<typename T>
+void bubble_sort(T arr[], int n) {
     for (int i = 0; i < n - 1; i++) {
         for (int j = 0; j < n - i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
-                std::swap(arr[j], arr[j + 1]);
+                swap(arr[j], arr[j + 1]);
             }
         }
     }
 }
 
-template <typename T>
-int binarySearch(T arr[], int n, T x) {
-    int left = 0, right = n - 1;
+template<typename T>
+int binary_search(T arr[], int n, const T& x) {
+    int left = 0;
+    int right = n - 1;
     while (left <= right) {
-        int mid = left + (right - left) / 2;
+        int mid = (left + right) / 2;
         if (arr[mid] == x) {
             return mid;
-        }
-        if (arr[mid] < x) {
-            left = mid + 1;
-        } else {
-            right = mid - 1;
-        }
-    }
-    return -1;
-}
-
-template <>
-int binarySearch<char*>(char* arr[], int n, char* x) {
-    bubbleSort(arr, n);
-    int left = 0, right = n - 1;
-    while (left <= right) {
-        int mid = left + (right - left) / 2;
-        int cmp = strcmp(arr[mid], x);
-        if (cmp == 0) {
-            return mid;
-        }
-        if (cmp < 0) {
+        } else if (arr[mid] < x) {
             left = mid + 1;
         } else {
             right = mid - 1;
@@ -50,18 +33,35 @@ int binarySearch<char*>(char* arr[], int n, char* x) {
 }
 
 int main() {
-    int arr[] = {10, 20, 30, 40, 50};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    int x = 30;
-    int result = binarySearch(arr, n, x);
-    (result == -1) ? std::cout << "Element is not present in array\n" 
-                   : std::cout << "Element is present at index " << result << '\n';
+    int arr1[m] = {5, 2, 9, 1, 5, 6, 8};
+    bubble_sort(arr1, m);
+    for (int i = 0; i < m; i++) {
+        cout << arr1[i] << " ";
+    }
+    cout << endl;
 
-    char* strArr[] = {"john", "alex", "doe", "jane", "smith"};
-    n = sizeof(strArr) / sizeof(strArr[0]);
-    char* str = "doe";
-    result = binarySearch(strArr, n, str);
-    (result == -1) ? std::cout << "Element is not present in array\n" 
-                   : std::cout << "Element is present at index " << result << '\n';
+    int x1 = 5;
+    int pos1 = binary_search(arr1, m, x1);
+    if (pos1 == -1) {
+        cout << "Element not found" << endl;
+    } else {
+        cout << "Element found at position " << pos1 << endl;
+    }
+
+    const char* arr2[m] = {"apple", "banana", "cherry", "grape", "orange", "pear", "strawberry"};
+    bubble_sort(arr2, m);
+    for (int i = 0; i < m; i++) {
+        cout << arr2[i] << " ";
+    }
+    cout << endl;
+
+    const char* x2 = "orange";
+    int pos2 = binary_search(arr2, m, x2);
+    if (pos2 == -1) {
+        cout << "Element not found" << endl;
+    } else {
+        cout << "Element found at position " << pos2 << endl;
+    }
+
     return 0;
 }
