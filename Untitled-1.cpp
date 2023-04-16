0a74648824cd055a34e959b59aba646735701e2a
Denis-Kalinyik <99794679+Denis-Kalinyik@users.noreply.github.com>
2023-04-16 23:42:25 +0300

    Add files via upload

--- Untitled-1.cpp
+++ Untitled-1.cpp
@@ -2,22 +2,25 @@
 #include <algorithm>
 #include <cstring>
 
+
+
+
 template<typename T>
 void find_min_and_count(const T* arr, const size_t size, T& min_value, size_t& count)
 {
     // Початкова кількість повторень дорівнює 0
     count = 0;
-    
+   
     // Якщо масив порожній, повертаємо порожні значення
     if (size == 0)
     {
         min_value = T();
         return;
     }
-    
+   
     // Знаходимо мінімальне значення в масиві за допомогою функції min_element
     min_value = *std::min_element(arr, arr + size);
-    
+   
     // Обчислюємо кількість повторень мінімального значення
     for (size_t i = 0; i < size; i++)
     {
@@ -28,17 +31,37 @@ void find_min_and_count(const T* arr, const size_t size, T& min_value, size_t& c
     }
 }
 
+
+
+
 int main()
 {
     const size_t size = 5;
-    char* arr[size] = {"bbb", "aaa", "ccc", "aaa", "ddd"};
-    char* min_value;
+    const char* arr[size] = {"bbb", "aaa", "ccc", "aaa", "ddd"};
+    const char* min_value;
     size_t count;
 
-    find_min_and_count(arr, size, min_value, count);
+
+
+
+    find_min_and_count<const char*>(arr, size, min_value, count);
+
+
+
+
+    std::cout << "Array: ";
+    for (size_t i = 0; i < size; i++)
+    {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+
 
     std::cout << "Minimum value: " << min_value << std::endl;
     std::cout << "Count of minimum value: " << count << std::endl;
+
+
+
 
     return 0;
 }
